0a4988ab9a18aa2b22c1ef548f9ece5d5465585a
Django-Blockchained <vishalp7711@gmail.com>
2023-11-04 13:34:56 +0530

    test

---
+++ my-app/src/products.jsx
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Air-Max Sneaker',
+    image: 'https://www.atipicishop.com/296937-thickbox_default/1000075342.jpg',
+    description:
+      'Air Max Unisex sneaker',
+    brand: 'Nike',
+    category: 'Shoes',
+    price: 5899,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Azdon Suede Loafer',
+    image: 'https://5.imimg.com/data5/SELLER/Default/2023/5/307451238/UX/JD/ZX/122953519/-mg-2374-1000x1000.JPG',
+    description:
+      'Sude leather loafers for formal wear and festive wear',
+    brand: 'Azdon',
+    category: 'Shoes',
+    price: 2599,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Converse Red',
+    image: 'https://cdn.laredoute.com/products/9/c/f/9cfd6264f37efdb7acb72e966ffebf15.jpg?width=500&dpr=1',
+    description:
+      'Converse Red Unisex',
+    brand: 'Convers',
+    category: 'Shoes',
+    price: 5929,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Woodland Trekking Shoe',
+    image: 'https://assets.woodlandworldwide.app/compressed/images/FGC0150R7481Z/KHAKI/FGC0150R7481Z_023_0.jpg',
+    description:
+      'Khaki Style trekking shoe',
+    brand: 'Woodland',
+    category: 'Shoes',
+    price: 3399,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Crocs Classic',
+    image: 'https://www.bfgcdn.com/1500_1500_90/702-2085-1911/crocs-kids-classic-clog-sandals.jpg',
+    description:
+      'Classis Crocs Clog Sandals',
+    brand: 'Crocs',
+    category: 'Sandals',
+    price: 4299,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Puma Mercedes Edition Slider',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjqndzDyqWfTn6JMYI581PLSItHVwvnD4D7TbOdL4qi5oP0eNd072XTWWpAQyM6s-PhjA&usqp=CAU",
+    description:
+      'Limited Mercedes edition comfy slider  ',
+    brand: 'Puma',
+    category: 'Sandals',
+    price: 1299,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
